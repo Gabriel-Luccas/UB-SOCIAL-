3e0bafd6458679d07abd4b9703db2c8b6ddbd111
Gabriel Costa Lucas <gcostaluccas95@gmail.com>
2024-09-27 19:24:03 -0300

    Paginator

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,32 +1,27 @@
 from rest_framework import serializers
 from .models import Curso, Avaliacao
 
+
 class AvaliacaoSerializer(serializers.ModelSerializer):
 
     class Meta:
-        extra_kwargs = {
-            'email': {'write_only': True}
-        }
+        extra_kwargs = {"email": {"write_only": True}}
         model = Avaliacao
         fields = (
-            'id',
-            'curso',
-            'nome',
-            'email',
-            'comentario',
-            'avaliacao',
-            'criacao',
-            'ativo'
+            "id",
+            "curso",
+            "nome",
+            "email",
+            "comentario",
+            "avaliacao",
+            "criacao",
+            "ativo",
         )
 
+
 class CursoSerializer(serializers.ModelSerializer):
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Curso
-        fields = (
-            'id',
-            'titulo',
-            'url',
-            'criacao',
-            'ativo'
-        )
+        fields = ("id", "titulo", "url", "criacao", "ativo")
